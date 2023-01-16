d197a90648d36a843701bbe96cc600b97ab49e39
lamnaourm <mohammed.lamnaour@gmail.com>
2023-01-16 14:51:53 +0100

    register

--- routes/app.js
+++ routes/app.js
@@ -10,6 +10,31 @@ const routes = express.Router()
 
 routes.post('/register', (req, res) => {
 
+    const {email, firstname, lastname, password} = req.body;
+
+    if(!email || !firstname || !lastname || !password)
+        return res.status(404).json({message: 'donnees user manquants'});
+
+    bcrypt.genSalt(10, (err, salt) => {
+        if(err)
+            return res.status(500).json({message: 'err generation salt'});
+
+        bcrypt.hash(password, salt, (err, hash) => {
+            if(err)
+                return res.status(500).json({message: 'err generation hash'});
+
+            const user = new userModel({...req.body, password:hash});
+
+            user.save((err, user) => {
+                if(err)
+                    return res.status(500).json({message: 'err save user'});
+
+                return res.status(200).json({message: 'user saved'});
+            })
+
+        })
+
+    })
 })
 
 routes.post('/login', (req, res) => {
